0ded086cadf64da84a3a371e03c598c409064c5a
Vyshnav.K <vyshnavk117@gmail.com>
2024-01-17 17:57:54 +0530

    Update nanoid version and add line item functionality

--- src/components/Invoice.jsx
+++ src/components/Invoice.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { nanoid } from 'nanoid'
 
 const Invoice = () => {
   const [logo, setLogo] = useState(null)
@@ -14,7 +15,45 @@ const Invoice = () => {
   const [invoiceNumber, setInvoiceNumber] = useState('')
   const [date, setDate] = useState('')
   const [dueDate, setdueDate] = useState('')
-    const [invoiceTitle, setInvoiceTitle] = useState('')
+  const [invoiceTitle, setInvoiceTitle] = useState('')
+    const [data, setData] = useState([])
+    const [addFormData, setAddFormData] = useState({
+      desc: '',
+      qty: '',
+      rate: '',
+      amount: '',
+    })
+
+      const handleAddFormChange = (event) => {
+        event.preventDefault()
+
+        const fieldName = event.target.getAttribute('name')
+        const fieldValue = event.target.value
+
+        const newFormData = { ...addFormData }
+        newFormData[fieldName] = fieldValue
+
+        setAddFormData(newFormData)
+      }
+
+      const handleAddFormSubmit = (event) => {
+        event.preventDefault()
+
+        const newData = {
+          id: nanoid(),
+          desc: addFormData.desc,
+          qty: addFormData.qty,
+          rate: addFormData.rate,
+          amount: addFormData.amount,
+        }
+        const newDataArr = [...data, newData]
+        document.getElementById('desc').value = ''
+        document.getElementById('qty').value = ''
+        document.getElementById('rate').value = ''
+        document.getElementById('amount').value = ''
+        setData(newDataArr)
+      }
+
 
   const handlePrint = () => {
     window.print()
@@ -96,16 +135,15 @@ const Invoice = () => {
             </div>
           </div>
           <div>
-                        <input
-                type='text'
-                className='heading'
-                placeholder='INVOICE'
-                value={invoiceTitle}
-                onChange={(e) => {
-                    setInvoiceTitle(e.target.value)
-                }}
+            <input
+              type='text'
+              className='heading'
+              placeholder='INVOICE'
+              value={invoiceTitle}
+              onChange={(e) => {
+                setInvoiceTitle(e.target.value)
+              }}
             />
-  
           </div>
         </div>
         <div className='header-two'>
@@ -191,6 +229,70 @@ const Invoice = () => {
                   }}
                 />
               </div>
+            </div>
+          </div>
+        </div>
+        <div className='container-two'>
+          <table className='table-container'>
+            <thead>
+              <tr>
+                <th>Item Description</th>
+                <th>Qty</th>
+                <th>Rate</th>
+                <th>Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((item, idx) => {
+                return (
+                  <tr key={idx}>
+                    <td>{item.desc}</td>
+                    <td>{item.qty}</td>
+                    <td>{item.rate}</td>
+                    <td>{item.amount}</td>
+                  </tr>
+                )
+              })}
+              <tr>
+                <td>
+                  <input
+                    type='text'
+                    name='desc'
+                    onChange={handleAddFormChange}
+                    id='desc'
+                  />
+                </td>
+                <td>
+                  <input
+                    type='text'
+                    name='qty'
+                    onChange={handleAddFormChange}
+                    id='qty'
+                  />
+                </td>
+                <td>
+                  <input
+                    type='text'
+                    name='rate'
+                    onChange={handleAddFormChange}
+                    id='rate'
+                  />
+                </td>
+                <td>
+                  <input
+                    type='text'
+                    name='amount'
+                    onChange={handleAddFormChange}
+                    id='amount'
+                  />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div>
+            <div className='btn' onClick={handleAddFormSubmit}>
+              <h5 className='plus'>+</h5>
+              <h5 className='plus-text'>Add Line Item</h5>
             </div>
           </div>
         </div>
